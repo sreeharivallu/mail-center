f8fcdb823440e5b29c12c7b8e0e90a12e874e206
sreehari vallu <sreehari vallu>
2019-08-04 21:52:33 +0530

    Handled mandatory env variables

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var bodyParser = require('body-parser');
 var cron = require('node-cron');
 var routes = require('./routes/routes');
 var mailServer = require('./controllers/mail-server-status');
-//const dotenv = require('dotenv');
-//dotenv.config();
+const dotenv = require('dotenv').config();
+
 var mailServerStatus = require('./controllers/mail-server-status');
 const config = require('./configs/config');
-console.log(config);
 
 const port = process.env.PORT || 3000;
 
@@ -19,19 +18,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-/*app.use(function (err, req, res, next) {
-    if (err.code === 'permission_denied') {
-      res.status(403).send('Forbidden');
-    }
-});
-*/
 app.get('/', (req, res) => {  
     res.send('Hello email manager for siteminder!')
 })
 
 app.use('/email', routes);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}!`);
-    cron.schedule('15 * * * * *', mailServer.setActiveMailServer);
-})
+
+// Ensure required ENV vars are set
+let requiredEnv = [
+    'mailgun_apikey', 'mailgun_domain', 'sendgrid_apikey'    
+];
+
+let unsetEnv = requiredEnv.filter((env) => !(typeof process.env[env] !== 'undefined'));
+  
+if (unsetEnv.length > 0) {
+    throw new Error("Required ENV variables are not set: [" + unsetEnv.join(', ') + "]");
+}else{
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}!`);
+        cron.schedule('15 * * * * *', mailServer.setActiveMailServer).start();
+    })
+}
+
